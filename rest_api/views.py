4d8ecb5c94cc5c4870ba9dc3624333e049d12138
Johnson <breaking.lover@gmail.com>
2020-02-29 15:41:41 -0500

    added permission to user authentication

--- rest_api/views.py
+++ rest_api/views.py
@@ -2,9 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+import rest_framework.authentication import TokenAuthentication
 
 from rest_api import serializers
-
+from rest_api import models, permissions
 
 
 class HelloApiView(APIView):
@@ -92,3 +93,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_class = (permissions.UpdateOwnProfile,)
